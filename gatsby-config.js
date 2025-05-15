2ede5c2bb9e5d113a8bc807db8ba86fe536fb167
Suhaas <sca5320@psu.edu>
2025-05-14 22:40:51 -0400

    changed naming

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Suhaas Achanta',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Suhaas Achanta is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://suhaasachanta.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@suhaasachanta',
   },
   plugins: [
     // 1. FIRST: All filesystem sources
@@ -110,8 +110,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Suhaas Achanta',
+        short_name: 'Suhaas Achanta',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
